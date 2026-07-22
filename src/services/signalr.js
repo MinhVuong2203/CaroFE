e281d30584d6d5c023216feaca26aad1a4ea6ad2
MinhVuong2203 <vuonghihihihi@gmail.com>
2026-07-22 19:27:22 +0700

    Update signalr.js

--- src/services/signalr.js
+++ src/services/signalr.js
@@ -1,6 +1,6 @@
 import * as signalR from '@microsoft/signalr'
 
 export const connection = new signalR.HubConnectionBuilder()
-  .withUrl(import.meta.env.VITE_SIGNALR_URL)
+  .withUrl('https://caro-production-097e.up.railway.app/gameHub')
   .withAutomaticReconnect()
   .build()
